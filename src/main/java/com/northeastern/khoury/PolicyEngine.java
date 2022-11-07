4460810fd533352b6a043b39f14ff8fe8938c5be
Ben Weintraub <ben@weintraub.xyz>
2022-11-07 11:39:37 -0500

    Parse simple policy

--- src/main/java/com/northeastern/khoury/PolicyEngine.java
+++ src/main/java/com/northeastern/khoury/PolicyEngine.java
@@ -1,52 +1,54 @@
 package com.northeastern.khoury;
 
-import gov.nist.csd.pm.pdp.memory.MemoryPDP;
-import gov.nist.csd.pm.pdp.PDP;
+import gov.nist.csd.pm.pap.PAP;
 import gov.nist.csd.pm.pap.memory.MemoryPAP;
+import gov.nist.csd.pm.pdp.PDP;
+import gov.nist.csd.pm.pdp.memory.MemoryPDP;
+import gov.nist.csd.pm.policy.author.pal.statement.PALStatement;
 import gov.nist.csd.pm.policy.exceptions.PMException;
-// import gov.nist.csd.pm.pdp.decider.Decider;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.logging.Logger;
 import java.util.logging.Level;
+import java.util.function.Supplier;
 
 public class PolicyEngine {
 
   static Logger logger = Logger.getLogger(PolicyEngine.class.getName());
 
-  // Graph graph = new MemGraph();
-  // private final Logger log = getLogger(getClass());
-  // Decider decider;
-
   public static void main(String[] args) {
+    // TODO make this a CLI param with a default of SEVERE
+    logger.setLevel(Level.FINE);
+
     if (args.length != 1) {
       System.err.printf("%d arguments provided. Need 1.", args.length);
       System.exit(1);
     }
 
-    // TODO make this a CLI param with a default of SEVERE
-    logger.setLevel(Level.FINE);
 
     // Read the policy from disk
     Path fileName = Path.of(args[0]);
     String policyString;
     try {
       policyString = Files.readString(fileName);
-      System.out.println(policyString);
+      logger.fine(() -> "Full policy:\n" + policyString);
     } catch (IOException io) {
-      logger.info("Problem reading file: " + io.getMessage());
+      logger.severe(() -> "Problem reading file: " + io.getMessage());
       return;
     }
 
+    PDP pdp;
     try {
-      PDP pdp = new MemoryPDP(new MemoryPAP());
+      PAP pap = new MemoryPAP();
+      List<PALStatement> pals = pap.compilePAL(policyString);
+      pdp = new MemoryPDP(pap);
     } catch (PMException pm) {
-      logger.info("Problem initializing PAP or PDP: " + pm.getMessage());
+      logger.severe(() -> "Problem initializing PAP or PDP: " + pm.getMessage());
       return;
     }
-    // List<PALStatement> pals = pdp.compilePAL(input);
 
 
     return;
