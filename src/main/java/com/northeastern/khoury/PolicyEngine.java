6a8d5906bfad81194ce3334f9e0a18bd312aca99
Ben Weintraub <ben@weintraub.xyz>
2022-11-09 16:37:45 -0500

    Execute test policy decisions

--- src/main/java/com/northeastern/khoury/PolicyEngine.java
+++ src/main/java/com/northeastern/khoury/PolicyEngine.java
@@ -29,7 +29,12 @@ public class PolicyEngine {
       System.exit(1);
     }
 
-    new PolicyEngine(args[0]);
+    PolicyEngine pe = new PolicyEngine(args[0]);
+    boolean b = pe.getPermission("U1", "O1", "permission0");
+    System.out.println("Can U1 do permission0 on O1? " + b);
+
+    b = pe.getPermission("NO", "O1", "permission0");
+    System.out.println("Can NO do permission0 on O1? " + b);
 
     return;
   }
