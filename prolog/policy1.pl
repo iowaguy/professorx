75947190d9e4fd8962833fa69a5646b51db1d495
Ben Weintraub <ben@weintraub.xyz>
2023-03-24 15:05:45 -0400

    Implement simplified prohibition model

--- prolog/policy1.pl
+++ prolog/policy1.pl
@@ -1,4 +1,23 @@
 %%%%% These Facts define a test policy. %%%%%%%
+u(u1).
+u(u2).
+ua(ua1).
+ua(ua2).
+ua(ua3).
+ua(ua4).
+ua(ua5).
+ua(ua6).
+ua(ua7).
+o(o1).
+o(o2).
+oa(oa1).
+oa(oa2).
+oa(oa3).
+oa(oa4).
+oa(oa5).
+oa(oa6).
+oa(oa7).
+pc(pc1).
 
 % User and user attribute assignments.
 assign(u1,ua7).
@@ -16,6 +35,7 @@ assign(ua6,ua4).
 
 % Object and object attribute assignments.
 assign(o1,oa7).
+assign(o2,oa5).
 assign(oa7,oa3).
 assign(oa7,oa6).
 assign(oa3,oa1).
@@ -29,10 +49,13 @@ association(ua4,oa5,[p0]).
 association(ua4,oa4,[p4]).
 association(ua4,oa2,[p4]).
 association(ua2,oa2,[p0]).
-%% association(ua1,o1,p0).
+association(ua1,oa7,[p1]).
 
 % Decisions, pre-mutation
 decision(u2, o1, p0, deny).
 decision(u1, o1, p0, grant).
 decision(u2, o1, p4, deny).
 decision(u1, o1, p4, grant).
+
+% Effect of the following: u2 cannot do p1 on o1
+disjunctiveProhibition(u2, [oa7], [p1]).
--- prolog/rules.pl
+++ prolog/rules.pl
@@ -3,10 +3,42 @@ isContained(X, Y) :-
     assign(X, Y);
     assign(X, Z),
     isContained(Z, Y).
+% constraints to add:
+% - only user attributes can contain users
+% - only object attributes can contain objects
 
 decide(U, O, AR) :-
     once((association(UA, OA, ARS),
           member(AR, ARS),
           isContained(U, UA),
-          isContained(O, OA))).
+          isContained(O, OA),
+          \+ disjProhibited(U, O, AR))).
 
+% Our prohibition model right now simulates a default
+% exclusion set which includes all policy classes.
+% In practice, this means that the only prohibted
+% attributes are those whose subtree root is included
+% in the inclusion set AT.
+disjProhibited(U, AT, AR) :-
+    % if there is a disjunctive prohibition such that
+    % the access right AR is in the prohibited set of
+    % access rights ARS, and the attribute being accessed
+    % is in the subtree of an element in the inclusion
+    % set.
+    disjunctiveProhibition(U_or_UA, ATI, ARS),
+
+    % The access right AR must be in the set of access
+    % rights ARS in the defined prohibition.
+    member(AR, ARS),
+
+    % Either the user U must unify with U_or_UA or be
+    % contained by it.
+    isContained(U, U_or_UA),
+
+    % The attribute being accessed AT must be in the
+    % inclusion set of the disjunctive range of ATI
+    inInclusionSet(AT, ATI).
+
+inInclusionSet(AT, [Head|Tail]) :-
+    isContained(AT, Head);
+    inInclusionSet(AT, Tail).
